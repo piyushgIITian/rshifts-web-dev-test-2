389b255885f02fe021866278d0866553ef90c3ed
piyushgiitian <piyushgautamtg@gmail.com>
2022-03-05 14:33:32 +0530

    added planets section

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,7 +7,7 @@ import "../style.css"
 import Panel1 from './landing-page/Panel1'
 import Panel2 from './landing-page/Panel2'
 import Panel3 from './landing-page/Panel3'
-// import Panel4 from './landing-page/Panel4'
+import Panel4 from './landing-page/Panel4'
 
 
 export default function LandingPage() {
@@ -22,12 +22,12 @@ export default function LandingPage() {
   }
 
   return (
-    <div style={{background:"white"}}>
+    <div className='wrap' style={{background:"white"}}>
       <Header buttonText={headerText} link={"/login"}/>
       <Panel1/>
       <Panel2/>  
       <Panel3/>  
-      {/* <Panel4/> */}
+      <Panel4/>
       <Footer/>
     </div>
   )
--- src/components/landing-page/Panel3.js
+++ src/components/landing-page/Panel3.js
@@ -6,41 +6,41 @@ import "../../style.css"
 export default function Panel3() {
   return (
       <div style={{position:"relative"}}>
-    <div className="panel-3 ">
-        <div className="p3-cards d-flex justify-content-center align-items-center ">
-        
-        <Card id="p3-card" className="p3-card-1">
+        <div className="panel-3 ">
+            <div className="p3-cards d-flex justify-content-center align-items-center ">
             
-            <Card.Body>
-           
-            <Card.Text>
-            Need controllers for VR. So VR is annoying!
-            </Card.Text>
-            </Card.Body>
+            <Card id="p3-card" className="p3-card-1">
+                
+                <Card.Body>
             
-        </Card>
-        <Card id="p3-card" className="p3-card-2">
+                <Card.Text>
+                Need controllers for VR. So VR is annoying!
+                </Card.Text>
+                </Card.Body>
+                
+            </Card>
+            <Card id="p3-card" className="p3-card-2">
+                
+                <Card.Body>
             
-            <Card.Body>
-           
-            <Card.Text>
-            Not an issue now, because we provide best hand tracking algorithms to make your phone the one and only controller you always had.
-            </Card.Text>
-            </Card.Body>
+                <Card.Text>
+                Not an issue now, because we provide best hand tracking algorithms to make your phone the one and only controller you always had.
+                </Card.Text>
+                </Card.Body>
+                
+            </Card>
+            <Card id="p3-card" className="p3-card-1">
+                
+                <Card.Body>
             
-        </Card>
-        <Card id="p3-card" className="p3-card-1">
-            
-            <Card.Body>
-           
-            <Card.Text>
-            Don’t hesitate now, register now and enter the RWorld at the earliest.
-            </Card.Text>
-            </Card.Body>
-            
-        </Card>
+                <Card.Text>
+                Don’t hesitate now, register now and enter the RWorld at the earliest.
+                </Card.Text>
+                </Card.Body>
+                
+            </Card>
+            </div>
         </div>
-    </div>
     <div className="panel-3-2">
         <div className="p3-2-text justify-content-center align-items-center ">
             <div className='panel-3-2-txt1'>
