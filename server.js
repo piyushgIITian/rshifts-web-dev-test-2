3b9bbd7e92a9d36a09aa46e2b4553d78250e49b5
piyushgiitian <piyushgautamtg@gmail.com>
2022-03-06 18:36:41 +0530

    nothing much 6-3-2022

--- server.js
+++ server.js
@@ -2,47 +2,52 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 require("dotenv").config();
+const app = express();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", //replace with your email provider
-    port: 465,
-    auth: {
-      user: "piyushgautamtg@gmail.com",
-      pass: "piyush123"
-    }
-  });
+  host: "smtp.gmail.com", //replace with your email provider
+  port: 587,
+  secure: false,
+  auth: {
+    user: "piyushgautamtg@gmail.com",
+    pass: "piyush123"
+  }
+});
 
-  // verify connection configuration
+// verify connection configuration
 transporter.verify(function(error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take our messages");
-    }
-  });
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
 
-  app.post('/send', (req, res, next) => {
-    var name = req.body.name
-    var email = req.body.email
-    var subject = req.body.subject
-    var message = req.body.message
-  
-    var mail = {
-      from: name,
-      to: "piyush.gautam@rshifts.com",
-      subject: subject,
-      text: message
+app.post('/send', (req, res, next) => {
+  var name = req.body.name
+  var email = req.body.email
+  var subject = req.body.subject
+  var message = req.body.message
+
+  var mail = {
+    from: name,
+    to: "piyush.gautam@rshifts.com",
+    subject: subject,
+    text: message
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
     }
-  
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        res.json({
-          status: 'fail'
-        })
-      } else {
-        res.json({
-         status: 'success'
-        })
-      }
-    })
   })
+})
+
+const PORT = 3001 || 8080
+app.listen(PORT, () => console.info(`server has started on ${PORT}`))
--- src/components/landing-page/subscribe.js
+++ src/components/landing-page/subscribe.js
@@ -4,20 +4,7 @@ import { Button } from 'react-bootstrap'
 
 export default function subscribe() {
   return (
-    <div style={{
-        width:"100%",
-        height:"400px",
-        fontFamily: "Poppins",
-        fontSize: "28px",
-        lineHeight: "48px",
-        color: "#1F1449",
-        display:"flex",
-        flexDirection:"column",
-        justifyContent:"center",
-        alignItems:"center",
-        textAlign:"center"
-
-    }}>
+    <div className="subscribe">
         Subscribe to our newsletter to get the latest updates of RShifts
         <div className="text-center ">
         <Button className="px-3 mt-3" style={{
