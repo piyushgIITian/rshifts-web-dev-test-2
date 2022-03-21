558e2b74ad3e0ef2ab692368c6e19c47f50f8880
piyushgiitian <piyushgautamtg@gmail.com>
2022-03-21 13:12:11 +0530

    small ui changes and added go to top

--- src/components/landing-page/Panel1.js
+++ src/components/landing-page/Panel1.js
@@ -7,61 +7,63 @@ import arrow from "../../Images/arrow-1.svg"
 import facebook from "../../Images/facebook.svg"
 import instagram from "../../Images/Instagram.svg"
 import linkedin from "../../Images/Linkedin.svg"
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 import "../../style.css"
-import {HideBetween, HideOn } from 'react-hide-on-scroll'
+import { HideBetween, HideOn } from 'react-hide-on-scroll'
 
 export default function Panel1() {
-  const {currentUser} = useAuth();
-  
+  const { currentUser } = useAuth();
+
   return (
     <div className="container-lp-1" id="home">
 
-        <div className='sidePanel' style={{alignItems:"center"}}>
-        <Image className="illus-arrow"src={arrow}/>
-        
+      <div className='sidePanel' style={{ alignItems: "center" }}>
+        <Image className="illus-arrow" src={arrow} />
+
         {/**
        * Social Media links
        */}
 
-          <div className="social-column">
-            <a href="https://www.facebook.com/rshifts"> <Image id="logo" className='hvr-float-shadow' src={facebook}/></a>
-            <a href="https://www.instagram.com/reality.shifts/"> <Image id="logo" className='hvr-float-shadow'  src={instagram}/></a>
-            <a href="https://www.linkedin.com/company/rshifts/"> <Image id="logo" className='hvr-float-shadow'  src={linkedin}/></a>
-          
-         
-          </div >
-        </div>
-        <div className="land-text font-poppins" >
+        <div className="social-column">
+          <a href="https://www.facebook.com/rshifts"> <Image id="logo" className='hvr-float-shadow' src={facebook} /></a>
+          <a href="https://www.instagram.com/reality.shifts/"> <Image id="logo" className='hvr-float-shadow' src={instagram} /></a>
+          <a href="https://www.linkedin.com/company/rshifts/"> <Image id="logo" className='hvr-float-shadow' src={linkedin} /></a>
+
+
+        </div >
+      </div>
+      <div className="land-text font-poppins" >
         <p id="join-line">Join the world’s most affordable</p>
         <p id="vr">VR</p>
         <p id="meta">METAVERSE</p>
-        <p id="description-p1">RShifts’ <a style={{color:"#AF4BF2"}}>RWorld</a> will be world’s first authentic VR Metaverse experience for mobile phones using cutting edge machine learning techniques to provide state of the art XR experience on the <a style={{color:"#EDBA3A"}}>GO !</a></p>
-        {!currentUser && 
-        <Button href="/signup"  className="mt-5 mb-5" id="rs-button" type="primary">
-              Join Us
-              
+        <p id="description-p1">RShifts’ <a style={{ color: "#AF4BF2" }}>RWorld</a> will be world’s first authentic VR Metaverse experience for mobile phones using cutting edge machine learning techniques to provide state of the art XR experience on the <a style={{ color: "#EDBA3A" }}>GO !</a></p>
+        {!currentUser &&
+          <Button href="/signup" className="mt-5 mb-5" id="rs-button" type="primary">
+            Join Us
+
           </Button>
         }
-        {currentUser && 
-        <Button  className="mt-5 mb-5" id="rs-button" type="primary">
-          <Link  to="our-team" spy={true} smooth={true} isDynamic={true} >
+        {currentUser &&
+          <Button className="mt-5 mb-5" id="rs-button" type="primary">
+            <Link to="our-team" spy={true} smooth={true} isDynamic={true} >
               Our Team
-          </Link>    
+            </Link>
           </Button>
         }
-        </div>
-        <source media="(min-width:800px)" srcSet={illus1}/>
-        <Image className="illus-1" src={illus1}/>
-          <div className="go-top">
+      </div>
+      <source media="(min-width:800px)" srcSet={illus1} />
+      <Image className="illus-1" src={illus1} />
+      <div className="go-top">
         <HideBetween div startDivID="rs-header" endDivID="home">
-        <div className="go-top-text" >
-        <a href="#rs-header" class="stretched-link"></a>
-          GO TOP
-        </div>
-      
-      </HideBetween>
+          <div className="go-top-text" >
+            
+              <Link to="rs-header" spy={true} smooth={true} isDynamic={true} >
+                GO TOP
+              </Link>
+            
+          </div>
+        </HideBetween>
       </div>
-      </div>
+    </div>
   )
 }
