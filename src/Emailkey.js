9a0f9727151815ab078400de85feba324f6ac270
piyushgiitian <piyushgautamtg@gmail.com>
2022-03-25 15:59:40 +0530

    used emailjs for contact us form

---
+++ src/Emailkey.js
@@ -0,0 +1,4 @@
+export default {
+    USER_ID: `ge_qVTfLMeCCeKkqH`, //userID
+    TEMPLATE_ID: `WAVZT2rQaOnFjPIFI7426`,         //templateID
+    }
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,7 @@
 import React, {useRef } from 'react'
 import { Form, Button, Card, Image } from "react-bootstrap"
-import axios from 'axios';
+// import axios from 'axios';
+import emailjs from "emailjs-com";
 
 
 export default function ContactUs() {
@@ -15,26 +16,37 @@ export default function ContactUs() {
     messageRef.current = "";
     subjectRef.current = "";
   }
-  async function submitEmail(e){
-    e.preventDefault();
-    console.log("you clicked submit");
-    console.log(nameRef.current.value);
-    axios.post('https://localhost:5000/send',{
-      name: nameRef.current.value,
-      email:emailRef.current.value,
-      subject:subjectRef.current.value,
-      message:messageRef.current.value,
-   })
-      .then((response)=>{
-        console.log("hello"+response.data)
-        if (response.data === 'done'){
-            alert("Message Sent."); 
-            resetForm()
-        }else {
-            alert("Message failed to send.")
-        }
-      })
-    }
+  async function handleSubmit(e){
+    e.preventDefault(); // Prevents default refresh by the browser
+
+    await emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, e.target, process.env.REACT_APP_EMAILJS_USER_ID)
+    .then((result) => {
+    alert("Message Sent, We will get back to you shortly", result.text);
+    },
+    (error) => {
+    alert("An error occurred, Please try again", error.text);
+    });
+    };
+  // async function submitEmail(e){
+  //   e.preventDefault();
+  //   console.log("you clicked submit");
+  //   console.log(nameRef.current.value);
+  //   axios.post('https://localhost:5000/send',{
+  //     name: nameRef.current.value,
+  //     email:emailRef.current.value,
+  //     subject:subjectRef.current.value,
+  //     message:messageRef.current.value,
+  //  })
+  //     .then((response)=>{
+  //       console.log("hello"+response.data)
+  //       if (response.data === 'done'){
+  //           alert("Message Sent."); 
+  //           resetForm()
+  //       }else {
+  //           alert("Message failed to send.")
+  //       }
+  //     })
+  //   }
 
   return (
     <div>
@@ -48,24 +60,24 @@ export default function ContactUs() {
       <Card.Body style={{borderRadius: "20px"}}>
         <h2 className="text-center mb-4" style={{fontSize: '22px', fontWeight: 'bold'}}>Get in touch with us!</h2>
       
-        <Form onSubmit={submitEmail}  >
+        <Form onSubmit={handleSubmit}  >
 
           <Form.Group id="name" className="mb-4" >
             
-            <Form.Control type="name" style={{height: "45px"}} placeholder = "Name"   
+            <Form.Control name="name" type="name" style={{height: "45px"}} placeholder = "Name"   
                                    ref={nameRef} required />
           </Form.Group>
           <Form.Group id="email" className="mb-4" >
             
-            <Form.Control type="email" style={{height: "45px"}} placeholder = "Email"  required  ref= {emailRef} />
+            <Form.Control type="email" name="email" style={{height: "45px"}} placeholder = "Email"  required  ref= {emailRef} />
           </Form.Group>
           <Form.Group id="subject" className="mb-4" >
             
-            <Form.Control type="subject" style={{height: "45px"}} placeholder = "Subject" ref={subjectRef} />
+            <Form.Control type="subject" name="subject" style={{height: "45px"}} placeholder = "Subject" ref={subjectRef} />
           </Form.Group>
           <Form.Group id="message" className="mb-4" >
             
-            <Form.Control type="message" style={{height: "45px"}} placeholder = "Message" required ref={messageRef} />
+            <Form.Control type="message" name="message" style={{height: "45px"}} placeholder = "Message" required ref={messageRef} />
           </Form.Group>
           <div className="text-center ">
           <Button className="px-3" id="rs-button" type="submit">
